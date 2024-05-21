c5f596533bab3d9f176854e4568d19e202befc1f
paulinetimoff <paulinetimoff@gmail.com>
2024-05-21 17:20:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="=utf-8">
+    <title>php</title>
+    </head>
+    <body>
+
+        <p>Привет,мир!</p>
+
+        <?php
+
+        $name="Polina";
+        $surname="Timofeyeva";
+        
+        
+        echo "My name:\n" . $name, " " . $surname;
+
+        ?>
+
+    </body> 
+</html>
